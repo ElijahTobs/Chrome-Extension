8140805d47a362df6766d5cdc5f6be54ac13e1ed
Comfort Ige <eligenius@gmail.com>
2023-05-21 22:19:02 +0100

    Add event listener for button

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
+})
+
