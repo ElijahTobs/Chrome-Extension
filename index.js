9a313c147233e515a0dc5212cf3ac17453f38f2d
Comfort Ige <eligenius@gmail.com>
2023-05-23 07:20:25 +0100

    Add renderLeads function

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  renderLeads()
 })
+
+
+function renderLeads() {
+  let listItems = ""
+  for (let x in myLeads){
+    listItems += "<li>" + myLeads[x] + "</li>"
+  }
+
+  ulEl.innerHTML = listItems
+}
 
