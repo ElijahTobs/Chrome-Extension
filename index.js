3aad1248097dd083881e6ad5afe27a9ceac6d786
Comfort Ige <eligenius@gmail.com>
2023-05-24 02:26:48 +0100

    Add delete button functionality

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-
-if (leadsFromLocalStorage.length > 0) {
+if (leadsFromLocalStorage.length) {
   myLeads = leadsFromLocalStorage
   renderLeads()
 }
 
-inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
+deleteBtn.addEventListener("dblclick", function(){
+  localStorage.clear()
+  myLeads = []
   renderLeads()
-  inputEl.value = ""
 })
 
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+  renderLeads()
+})
 
-// localStorage.clear()
 function renderLeads() {
   let listItems = ""
   for (let x in myLeads){
     listItems += `
       <li>
-        <a target="_blank" href="https://${myLeads[x]}"> ${myLeads[x]}
-      </li>`
+        <a target="_blank" href="https://${myLeads[x]}">
+          ${myLeads[x]}
+        </a>
+      </li>
+    `
   }
-
   ulEl.innerHTML = listItems
 }
 
